8893f6cdc2bac7a74e34673575bfc4445168dec6
chris <chris@dcdc9c1a-680d-0410-b355-e913d528223f>
2004-06-02 22:03:42 +0000

    initial
    git-svn-id: file:///Users/chris/import/svn/trunk@8 dcdc9c1a-680d-0410-b355-e913d528223f

---
+++ monoboot/monoboot.c
@@ -0,0 +1,12 @@
+
+/*
+ * monoboot
+ *
+ * chooses a monoimage to boot. 
+ *
+ * looks at the boot config file for the preferred image, then at the boot log
+ * to decide if we should go for the fallback image. then kexec-load the image,
+ * log our decision, make sure we're mounted read-only and then kexec the image
+ * 
+ */
+
