004533cbc103890c3bcd2b4bd4413ce589252d52
chris <chris@dcdc9c1a-680d-0410-b355-e913d528223f>
2004-11-28 16:15:44 +0000

    check if we have either /usr/bin/fakeroot or are really root
    git-svn-id: file:///Users/chris/import/svn/trunk@88 dcdc9c1a-680d-0410-b355-e913d528223f

---
+++ initrd/be-root.sh
@@ -0,0 +1,20 @@
+#!/bin/sh -e
+
+SCRIPT=$1
+FAKEROOT=/usr/bin/fakeroot
+if [ ! -x $FAKEROOT ]; then
+    USER=`whoami`
+    if [ "$USER" != "root" ]; then
+	echo "you must be root, or have $FAKEROOT"
+	exit 1;
+    fi
+    USEFAKEROOT=0
+else
+    USEFAKEROOT=1
+fi
+
+if [ $USEFAKEROOT = 1 ]; then
+    $FAKEROOT $SCRIPT
+else 
+    /bin/sh $SCRIPT
+fi
