d3461ef980590fa707f076b2bcc85a120143ce6b
chris <chris@dcdc9c1a-680d-0410-b355-e913d528223f>
2005-12-24 18:24:31 +0000

    Initial revision
    git-svn-id: file:///Users/chris/import/svn/trunk@134 dcdc9c1a-680d-0410-b355-e913d528223f

---
+++ libcli/clitest.c
@@ -0,0 +1,155 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <string.h>
+#include <unistd.h>
+#include "libcli.h"
+
+#define CLITEST_PORT		8000
+
+#define MODE_CONFIG_INT		10
+
+int cmd_test(struct cli_def *cli, char *command, char *argv[], int argc)
+{
+    int i;
+    cli_print(cli, "called %s with \"%s\"", __FUNCTION__, command);
+    cli_print(cli, "%d arguments:", argc);
+    for (i = 0; i < argc; i++)
+    {
+	cli_print(cli, "	%s", argv[i]);
+    }
+    return CLI_OK;
+}
+
+int cmd_set(struct cli_def *cli, char *command, char *argv[], int argc)
+{
+    if (argc < 2)
+    {
+	cli_print(cli, "Specify a variable to set");
+	return CLI_OK;
+    }
+    cli_print(cli, "Setting \"%s\" to \"%s\"", argv[0], argv[1]);
+    return CLI_OK;
+}
+
+int cmd_config_int(struct cli_def *cli, char *command, char *argv[], int argc)
+{
+	if (argc < 1)
+	{
+		cli_print(cli, "Specify an interface to configure");
+		return CLI_OK;
+	}
+	if (strcmp(argv[0], "?") == 0)
+	{
+		cli_print(cli, "  test0/0");
+	}
+	else if (strcasecmp(argv[0], "test0/0") == 0)
+	{
+		cli_set_configmode(cli, MODE_CONFIG_INT, "test");
+	}
+	else
+		cli_print(cli, "Unknown interface %s", argv[0]);
+	return CLI_OK;
+}
+
+int cmd_config_int_exit(struct cli_def *cli, char *command, char *argv[], int argc)
+{
+	cli_set_configmode(cli, MODE_CONFIG, NULL);
+	return CLI_OK;
+}
+
+int check_auth(char *username, char *password)
+{
+    if (!strcasecmp(username, "fred") && !strcasecmp(password, "nerk"))
+	return 1;
+    return 0;
+}
+
+int check_enable(char *password)
+{
+    if (!strcasecmp(password, "topsecret"))
+        return 1;
+    return 0;
+}
+
+void pc(struct cli_def *cli, char *string)
+{
+	printf("%s\n", string);
+}
+
+int main(int argc, char *argv[])
+{
+    struct cli_command *c;
+    struct cli_def *cli;
+    int s, x;
+    struct sockaddr_in servaddr;
+    int on = 1;
+
+    cli = cli_init();
+    cli_set_banner(cli, "libcli test environment");
+    cli_set_hostname(cli, "router");
+    cli_register_command(cli, NULL, "test", cmd_test, PRIVILEGE_UNPRIVILEGED, MODE_EXEC, NULL);
+    cli_register_command(cli, NULL, "sex",  NULL, PRIVILEGE_UNPRIVILEGED, MODE_EXEC, NULL);
+    cli_register_command(cli, NULL, "simple",  NULL, PRIVILEGE_UNPRIVILEGED, MODE_EXEC, NULL);
+    cli_register_command(cli, NULL, "simon",  NULL, PRIVILEGE_UNPRIVILEGED, MODE_EXEC, NULL);
+    cli_register_command(cli, NULL, "set", cmd_set,  PRIVILEGE_PRIVILEGED, MODE_EXEC, NULL);
+    c = cli_register_command(cli, NULL, "show", NULL,  PRIVILEGE_UNPRIVILEGED, MODE_EXEC, NULL);
+    cli_register_command(cli, c, "counters", cmd_test, PRIVILEGE_UNPRIVILEGED, MODE_EXEC, "Show the counters that the system uses");
+    cli_register_command(cli, c, "junk", cmd_test, PRIVILEGE_UNPRIVILEGED, MODE_EXEC, NULL);
+
+
+    cli_register_command(cli, NULL, "interface", cmd_config_int, PRIVILEGE_PRIVILEGED, MODE_CONFIG, "Configure an interface");
+    cli_register_command(cli, NULL, "exit", cmd_config_int_exit, PRIVILEGE_PRIVILEGED, MODE_CONFIG_INT, "Exit from interface configuration");
+    cli_register_command(cli, NULL, "address", cmd_test, PRIVILEGE_PRIVILEGED, MODE_CONFIG_INT, "Set IP address");
+
+    cli_set_auth_callback(cli, check_auth);
+    cli_set_enable_callback(cli, check_enable);
+    // Test reading from a file
+    {
+	    FILE *fh;
+
+	    if ((fh = fopen("clitest.txt", "r")))
+	    {
+		    // This sets a callback which just displays the cli_print() text to stdout
+		    cli_print_callback(cli, pc);
+		    cli_file(cli, fh, PRIVILEGE_UNPRIVILEGED, MODE_EXEC);
+		    cli_print_callback(cli, NULL);
+		    fclose(fh);
+	    }
+    }
+
+    if ((s = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+    {
+	perror("socket");
+	return 1;
+    }
+    setsockopt(s, SOL_SOCKET, SO_REUSEADDR, &on, sizeof(on));
+
+    memset(&servaddr, 0, sizeof(servaddr));
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    servaddr.sin_port = htons(CLITEST_PORT);
+    if (bind(s, (struct sockaddr *)&servaddr, sizeof(servaddr)) < 0)
+    {
+	perror("bind");
+	return 1;
+    }
+
+    if (listen(s, 50) < 0)
+    {
+	perror("listen");
+	return 1;
+    }
+
+    printf("Listening on port %d\n", CLITEST_PORT);
+    while ((x = accept(s, NULL, 0)))
+    {
+	cli_loop(cli, x);
+	close(x);
+    }
+
+    cli_done(cli);
+    return 0;
+}
+
---
+++ libcli/libcli.spec
@@ -0,0 +1,101 @@
+Version: 1.8.5
+Summary: Cisco-like telnet command-line library
+Name: libcli
+Release: 1
+Copyright: LGPL
+Group: Library/Communication
+Source: %{name}-%{version}.tar.gz
+URL: http://www.sf.net/projects/libcli
+Packager: David Parrish <david@dparrish.com>
+BuildRoot: /var/tmp/%{name}-buildroot/
+
+%description
+libcli provides a shared library for including a Cisco-like command-line
+interface into other software. It's a telnet interface which supports
+command-line editing, history, authentication and callbacks for a
+user-definable function tree.
+
+%prep
+%setup
+
+%build
+make
+
+%install
+rm -rf $RPM_BUILD_ROOT
+make DESTDIR=$RPM_BUILD_ROOT PREFIX=/usr install
+find $RPM_BUILD_ROOT/usr ! -type d -print | grep -v '\/(README|\.html)$' | \
+    sed "s@^$RPM_BUILD_ROOT@@g" | sed "s/^\(.*\)$/\1\*/" > %{name}-%{version}-filelist
+
+%post
+ldconfig
+
+%clean
+rm -rf $RPM_BUILD_ROOT
+
+%files -f %{name}-%{version}-filelist
+%defattr(-, root, root)
+
+%doc README Doc/usersguide.html Doc/developers.html
+
+%changelog
+* Mon May  2 2005 Brendan O'Dea <bod@optusnet.com.au> 1.8.5-1
+- Add cli_error function which does not filter output.
+
+* Wed Jan  5 2005 Brendan O'Dea <bod@optusnet.com.au> 1.8.4-1
+- Add printf attribute to cli_print prototype
+
+* Fri Nov 19 2004 Brendan O'Dea <bod@optusnet.com.au> 1.8.3-1
+- Free help if set in cli_unregister_command (reported by Jung-Che Vincent Li)
+- Correct auth_callback() documentation (reported by Serge B. Khvatov)
+
+* Thu Nov 11 2004 Brendan O'Dea <bod@optusnet.com.au> 1.8.2-1
+- Allow config commands to exit a submode
+- Make "exit" work in exec/config/submodes
+- Add ^K (kill to EOL)
+
+* Mon Jul 12 2004 Brendan O'Dea <bod@optusnet.com.au> 1.8.1-1
+- Documentation update.
+- Allow NULL or "" to be passed to cli_set_banner() and
+  cli_set_hostname() to clear a previous value.
+
+* Sun Jul 11 2004 Brendan O'Dea <bod@optusnet.com.au> 1.8.0-1
+- Dropped prompt arg from cli_loop now that prompt is set by
+  hostname/mode/priv level; bump soname.  Fixes ^L and ^A.
+- Reworked parsing/filters to allow multiple filters (cmd|inc X|count).
+- Made "grep" use regex, added -i, -v and -e args.
+- Added "egrep" filter.
+- Added "exclude" filter.
+
+* Fri Jul  2 2004 Brendan O'Dea <bod@optusnet.com.au> 1.7.0-1
+- Add mode argument to cli_file(), bump soname.
+- Return old value from cli_set_privilege(), cli_set_configmode().
+
+* Fri Jun 25 2004 Brendan O'Dea <bod@optusnet.com.au> 1.6.2-1
+- Small cosmetic changes to output.
+- Exiting configure/^Z shouldn't disable.
+- Support encrypted password.
+
+* Fri Jun 25 2004 David Parrish <david@dparrish.com> 1.6.0
+- Add support for privilege levels and nested config levels. Thanks to Friedhelm
+  Düsterhöft for most of the code.
+
+* Tue Feb 24 2004 David Parrish <david@dparrish.com>
+- Add cli_print_callback() for overloading the output
+- Don't pass around the FILE * handle anymore, it's in the cli_def struct anyway
+- Add cli_file() to execute every line read from a file handle
+- Add filter_count
+
+* Sat Feb 14 2004 Brendan O'Dea <bod@optusnet.com.au> 1.4.0-1
+- Add more line editing support: ^W, ^A, ^E, ^P, ^N, ^F, ^B
+- Modify cli_print() to add \r\n and to split on \n to allow inc/begin
+  to work with multi-line output (note:  API change, client code
+  should not include trailing \r\n; version bump)
+- Use libcli.so.M.m as the soname
+
+* Fri Jul 25 2003 David Parrish <david@dparrish.com>
+- Add cli_regular to enable regular processing while cli is connected
+
+* Wed Jun 25 2003 David Parrish <david@dparrish.com>
+- Stop random stack smashing in cli_command_name.
+- Stop memory leak by allocating static variable in cli_command_name.
